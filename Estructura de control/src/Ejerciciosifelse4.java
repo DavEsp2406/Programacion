8a6af3cca52628915c6927eb977dba342b3d7b0c
DavEsp2406 <david.espeton.aparacio@colegiolitterator.com>
2023-10-16 17:40:52 +0200

    ejercicios if else completos

--- Estructura de control/src/Ejerciciosifelse4.java
+++ Estructura de control/src/Ejerciciosifelse4.java
@@ -1,16 +1,32 @@
 import java.util.Scanner;
+import java.util.Calendar;
+
 
 public class Ejerciciosifelse4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Introduce la fecha de nacimiento");
+		System.out.println("Introduce tu año de nacimiento");
 		
 		Scanner sc = new Scanner (System.in);
 		
-		int num = sc.nextInt();
-
+		int naci = sc.nextInt();
+		
+		Calendar cal= Calendar.getInstance();
+		
+        int year= cal.get(Calendar.YEAR);
+        
+        
+        
+        if(naci <= year){
+        	System.out.println("Tu edad actual es " + (year - naci));
+        	
+        }else {
+        	System.out.println("La fecha de nacimiento es erronea");
+        }
+        
+ 
 	}
 
 }
---
+++ Estructura de control/src/Ejerciciosifelse5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ejerciciosifelse5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+System.out.println("Introduzca un número");
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int num = sc.nextInt();
+		
+		if(num >= 1 && num <= 100) {
+			System.out.println("El número esta dentro del rango");
+		}else {
+			System.out.println("El número no esta dentro del rango");
+		}
+
+	}
+
+}
